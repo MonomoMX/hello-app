d4bb0e55847e6ae7a7600a3635e22d0949d7775e
MonomoMX <152469348+MonomoMX@users.noreply.github.com>
2024-03-06 23:44:24 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,6 @@ st.file_uploader('Cargar un archivo')
 # Use widgets' returned values in variables
 for i in range(int(st.number_input('Num:'))): foo()
 if st.sidebar.selectbox('I:',['f']) == 'f': b()
-my_slider_val = st.slider('Quinn Mallory', 1, 88)
+my_slider_val = st.slider('Quinn x Mallory', 1, 88)
 st.write(slider_val)
 
